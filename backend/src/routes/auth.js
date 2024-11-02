5c160f4a5882c4a650b6b0e3903b139fa9a20661
Indrajit sahani <indrajit38mig@gmail.com>
2024-11-02 19:30:14 +0530

    added /api/v1/user/auth/login route

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -4,6 +4,7 @@ import jwt from 'jsonwebtoken'
 
 import { registerBody } from '../../zod/registerBody.js';
 import User from '../../db/userRegistration.js'
+import { loginBody } from '../../zod/loginBody.js';
 
 const authRouter = Router();
 
@@ -57,7 +58,55 @@ authRouter.post('/register', async (req, res) => {
         })
     }
 
-
 })
 
+authRouter.post('/login', async (req, res) => {
+    const { data } = loginBody.safeParse(req.body);
+
+    if (!data) {
+        return res.status(411).json({
+            error: "Fill all data/ Incorrect data type"
+        })
+    }
+    try {
+        const existingUser = await User.findOne({
+            username: req.body.username
+        })
+
+        if (!existingUser) {
+            return res.status(403).json({
+                error: "user does't exist"
+            });
+        }
+
+        const passwordVerify = await bycrpt.compare(req.body.password, existingUser.password);
+        if (!passwordVerify) {
+            return res.status(403).json({
+                error: "Invalid credentials"
+            });
+        }
+
+        const payLoad = {
+            username: existingUser.username,
+            userID: existingUser._id
+
+        }
+        const token = jwt.sign(payLoad, process.env.JWT_PASSWORD);
+
+        return res.cookie("access_token", token, {
+            expires: new Date(Date.now() + 250000000),
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: 'None'
+        }).status(200).json({
+            msg: "Login success"
+        })
+
+    } catch (err) {
+        return res.status(500).json({
+            error: err.message || err.toString()
+        })
+    }
+
+})
 export default authRouter
