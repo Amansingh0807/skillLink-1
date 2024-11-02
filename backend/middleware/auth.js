04a2646101bcd234177f9e7157e4e6cedd1ac8af
Indrajit sahani <indrajit38mig@gmail.com>
2024-11-02 19:47:24 +0530

    added auth middleware

---
+++ backend/middleware/auth.js
@@ -0,0 +1,30 @@
+import jwt from 'jsonwebtoken'
+
+export default function auth(req, res, next) {
+
+    const authHeader = req.cookies.access_token
+
+    if (!authHeader) {
+        return res.status(401).json({
+            msg: "Not valid form of token"
+        });
+    }
+
+    try {
+        const decoded = jwt.verify(authHeader, process.env.JWT_PASSWORD);
+
+        if (!decoded) {
+            return res.status(401).json({
+                msg: "not decoded"
+            })
+        }
+        req.userId = decoded.userID;
+        next();
+    }
+    catch (err) {
+        return res.status(500).json({
+            error: err.message || err.toString()
+        })
+    }
+
+}
